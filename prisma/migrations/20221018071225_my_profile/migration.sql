428fb2d422238fb008cc3b7b2d0fd89b3974cc9d
Joy <Joy987654321@gmail.com>
2022-10-20 14:49:39 +0530

    filed of db deleted

--- prisma/migrations/20221018071225_my_profile/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE `MyProfile` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `skill` VARCHAR(255) NOT NULL,
-    `about` VARCHAR(255) NOT NULL,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
