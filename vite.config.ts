2922b80d5285103fb64ed7d2ed60ef0274f8ce72
Joy Karmakar <96533682+joy095@users.noreply.github.com>
2024-12-29 00:22:43 +0530

    remove gsap

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	optimizeDeps: {
-		include: ['gsap', 'gsap/ScrollTrigger']
-	},
-	ssr: {
-		noExternal: ['gsap']
-	},
 	preview: {
 		host: '0.0.0.0', // Make the app accessible on all network interfaces
 		port: 4000 // Change this to any port you'd like for production preview
